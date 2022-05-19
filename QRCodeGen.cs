d4e1815707567143a34fbd31fc48818c2381b47a
mdabrowski25 <m.dabrowski97@o2.pl>
2022-05-19 18:02:53 +0200

    qrcode generator

---
+++ QRCodeGen.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Net.Codecrete.QrCodeGenerator;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace Company.Function
+{
+    public static class QRCodeGen
+    {
+        [FunctionName("Form")]
+        public static HttpResponseMessage Form([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
+        {
+            string indexPage = File.ReadAllText(context.FunctionAppDirectory + "/www/index.html");
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+
+            result.Content = new ByteArrayContent(System.Text.Encoding.UTF8.GetBytes(indexPage));
+
+            return result;
+
+        }
+
+        [FunctionName("QRCodeGen")]
+        public static async Task<IActionResult> Generate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string url = req.Query["url"];
+
+            log.LogInformation("Generating QR Code for url" + url);
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            url = url ?? data?.url;
+
+            if (string.IsNullOrEmpty(url))
+            {
+                return new BadRequestResult();
+            }
+
+            var qr = QrCode.EncodeText(url, QrCode.Ecc.Medium);
+            // convert it into a byte array for PNG output
+            var pngout = qr.ToPng(10, 1, SkiaSharp.SKColors.Black, SkiaSharp.SKColors.White);
+
+            // create a new return object
+            var ourResult = new ReturnObject { };
+
+            // store our byte array as a string 
+            ourResult.Image = Convert.ToBase64String(pngout);
+
+            // send it as JSON
+            return new JsonResult(ourResult);
+        }
+    }
+    public class ReturnObject
+    {
+        // the only property here is a string for the PNG
+        public string Image { get; set; }
+    }
+}
